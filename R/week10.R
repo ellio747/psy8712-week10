2328396bd72285ca4f1d3b96b155b1078e6ddf6d
Ben Elliott <ellio747@umn.edu>
2026-04-04 13:22:07 -0500

    working on xgb model

--- R/week10.R
+++ R/week10.R
@@ -10,19 +10,94 @@ gss_tbl <- read_sav("../data/GSS2016.sav", user_na = TRUE) %>%
   filter(!is.na(mosthrs)) %>% 
   rename(`work hours` = mosthrs) %>% 
   select(-c(hrs1, hrs2)) %>% 
-  select(where(function(x) mean(is.na(x)) < 0.75))
+  select(which(colMeans(is.na(.)) < 0.75)) %>% 
+  mutate(across(where(is.character), as.factor)) %>% 
+  mutate(`work hours` = as.numeric(`work hours`)) %>% 
+  as_tibble()
 
 # Visualization
 ggplot(gss_tbl, aes(x = `work hours`)) + 
   geom_histogram()
 
 # Analysis
-model <- train(
-  `work hours` ~ .,
-  data = gss_tbl,
-  method = "rf", 
-  preProcess = "medianImpute"
+
+## Define training and test sets
+set.seed(123)
+holdout_indices <- createDataPartition(gss_tbl$`work hours`, p = .75, list=F)
+gss_training <- gss_tbl[-holdout_indices,]
+gss_holdout <- gss_tbl[holdout_indices,]
+
+## Define consistent folds 
+cv_control <- trainControl(
+  method="cv", 
+  number=10, 
+  verboseIter = T
 )
+
+## Decide on hyperparameters to look at
+tune_grid <- expand.grid(
+  alpha = c(0,1),
+  lambda = seq(0.0001, 0.1, length = 10) # ,
+  # mtry = c(2, 3, 4, 5, 10, 20)
+)
+
+## Which Models Running
+mods <- c("lm", "glmnet", "ranger", "xgbLinear")
+
+## Run k-fold testing hyperparameters on training set
+
+# Four model types: #modelLookup(models)
+# OLS regression = "lm" no tuning parameters
+# elastic net = "glmnet" alpha and lambda (tuning parameters)
+# random forest = "ranger" mtry, splitrule, min.nod.size
+# “eXtreme Gradient Boosting” = xgbLinear alpha and lambda nrounds
+
+model1 <- train(
+  `work hours` ~ .,
+  gss_training,
+  na.action = na.pass,
+  method = "lm",
+  preProcess = c("medianImpute","center","scale"),
+  trControl=cv_control
+  )
+model1
+
+model2 <- train(
+  `work hours` ~ .,
+  gss_training,
+  na.action = na.pass,
+  method = "glmnet",
+  preProcess = c("medianImpute","center","scale"),
+  tuneGrid = tune_grid,
+  trControl = cv_control
+)
+model2
+
+model3 <- train(
+  `work hours` ~ .,
+  gss_training,
+  na.action = na.pass,
+  method = "ranger",
+  preProcess = c("medianImpute","center","scale"),
+  tuneGrid = tune_grid,
+  trControl=cv_control
+)
+model3
+
+model4 <- train(
+  `work hours` ~ .,
+  gss_training,
+  na.action = na.pass,
+  method = "xgbLinear",
+  preProcess = c("medianImpute","center","scale"),
+  tuneGrid = tune_grid,
+  trControl=cv_control
+)
+model3
+
+## Examine k-fold CV results
+
+## Compare to holdout CV results
 
 # Publication
 table1_tbl <- tibble(
